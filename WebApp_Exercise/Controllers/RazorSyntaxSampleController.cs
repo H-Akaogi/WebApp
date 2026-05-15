7689a9bd1f7d2a3516e4234bcdcec009e8b04512
H-Akaogi <hazumi.akaogi@wemex.com>
2026-05-15 13:54:41 +0900

    finished List4-4

--- WebApp_Exercise/Controllers/RazorSyntaxSampleController.cs
+++ WebApp_Exercise/Controllers/RazorSyntaxSampleController.cs
@@ -5,19 +5,20 @@ using Microsoft.AspNetCore.Mvc;
 /// </summary>
 [Route("Razor")]
 public class RazorSyntaxSampleController : Controller
+/// クラス名RazorSyntaxSampleまでをフォルダ名にしてViewの下に作る
 {
     /// <summary>
     /// ViewModel SampleFormのListをRazor View Show.cshtmlに渡す
     /// </summary>
     /// <returns></returns>
     [HttpGet("ShowData")]
-    public ActionResult Show()
+    public ActionResult Show()//Method名ShowをファイルにしてRazorSyntaxSampleフォルダの中に作成する
     {
         var list = new List<SampleForm>();
         list.Add(new SampleForm { Name = "山田太郎", Age = 25 });
         list.Add(new SampleForm { Name = "鈴木花子", Age = 23 });
         list.Add(new SampleForm { Name = "田中次郎", Age = 26 });
         list.Add(new SampleForm { Name = "佐藤かおり", Age = 25 });
-        return View(list);
+        return View(list);// アクション名と同名のビューに、ViewModel を渡して利用する
     }
 }
