163c6050056b835a5435678531db52c3f7d8ba35
H-Akaogi <hazumi.akaogi@wemex.com>
2026-05-18 11:18:49 +0900

    finished Ex12

--- WebApp_Exercise/Controllers/Ex09Controller.cs
+++ WebApp_Exercise/Controllers/Ex09Controller.cs
@@ -1,3 +1,4 @@
+using System.Text.Json; //Json使うので追加
 using Microsoft.AspNetCore.Mvc;
 /// <summary>
 /// 演習-09 HTML Formを作成するタグヘルパーを利用する
@@ -15,22 +16,50 @@ public class Ex09Controller : Controller
         var form = new Exercise07Form();
         return View(form);
     }
+
     /// <summary>
     /// [計算]ボタンクリックアクション
     /// </summary>
     /// <returns></returns>
-    [HttpPost("Result")]
-    public IActionResult Result(Exercise07Form form)
+
+    /// 保存側(POST)
+    [HttpPost("Calc")]//Post要求をTempDataによってGet要求に置き換える
+    public IActionResult Calc(Exercise07Form form)
     {
         // バリデーションチェック
         if (!ModelState.IsValid)
         {
+            // バリデーションエラーの場合入力画面を表示する
             return View("Enter", form);
         }
-        form.Answer = form.Value1 + form.Value2;
-        return View(form);
+        // Exercise07Formをシリアライズする
+        var json = JsonSerializer.Serialize(form);
+        // TempDataに登録する(一時的に保存。すぐ忘れる)
+        // TempDataにJSON文字列を一時保存している処理
+        TempData["Exercise07Form"] = json;
+        // 計算結果を表示するResultへリダイレクトする
+        return RedirectToAction("Result");
     }
 
+    /// <summary>
+    ///  受け取り側(GET)
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("Result")]
+    public IActionResult Result()
+    {
+        // TempDataからERxercise07Formを取り出す
+        string? json = (string)TempData["Exercise07Form"]!;
+        if (string.IsNullOrEmpty(json))
+        {
+            // TempDataにExercise07Formが無い場合、入力画面表示にリダイレクトする
+            return RedirectToAction("Enter");
+        }
+        // 存在する場合はデシリアライズする
+        var form = JsonSerializer.Deserialize<Exercise07Form>(json);
+        form!.Answer = form.Value1 + form.Value2;
+        return View(form);
+    }
     /// <summary>
     /// [戻る]ボタンクリックアクション
     /// </summary>
